e408892c63dceb7b02f882b529c3d85b0328a1a2
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-04-07 18:03:09 +0500

    Add files via upload

---
+++ assignment 1/app.js
@@ -0,0 +1,128 @@
+// // Qno 1
+
+// let num1 = +prompt("enter your number")
+// let num2 = +prompt("enter your number")
+
+// if(num1 > num2){
+//     console.log("The larger number is: " + num1 )
+
+// }
+// else if(num2 > num1){
+//     console.log("The larger number is: " + num2)
+
+// }
+// else{
+//     console.log("The two numbers are Equal")
+
+// }
+
+
+// // Qno 2
+
+// let num = +prompt("Enter a number")
+
+// if (num > 0 ) {
+//     alert("The sign is +")
+
+// }
+// else if (num < 0 ) {
+//     alert("The sign is -")
+// }
+// else{
+//     alert("The number is zero")
+// }
+
+
+// // Qno 3
+// let num1 = prompt("Enter the first number:");
+// let num2 = prompt("Enter the second number:");
+// let num3 = prompt("Enter the third number:");
+// let num4 = prompt("Enter the fourth number:");
+// let num5 = prompt("Enter the fifth number:");
+
+// let max = num1;
+
+// if (num2 > max) {
+//   max = num2;
+// }
+
+// if (num3 > max) {
+//   max = num3;
+// }
+
+// if (num4 > max) {
+//   max = num4;
+// }
+
+// if (num5 > max) {
+//   max = num5;
+// }
+
+// console.log("The largest number is: " + max);
+
+
+// Q no4
+
+// for(let i = 1; i <= 15; i++){
+//     if(i % 2 === 0){
+//         console.log(i + " is even");
+//     }
+//     else{console.log(i + " is odd")};
+
+// }
+
+// // Qno 5
+
+// let marks = +prompt("Enter your Percentge")
+
+// if (marks > 90) {
+//     console.log("The grade is A+")
+
+// }
+// else if (marks > 80  ) {
+//     console.log("The grade is A")
+
+// }   
+// else if (marks > 70  ) {
+//     console.log("The grade is B")
+
+// }   
+// else if (marks > 60 ) {
+//     console.log("The grade is C")
+
+// }   
+// else if (marks > 50  ) {
+//     console.log("The grade is D")
+
+// }   
+
+// else{
+//     console.log("You Are Fail !!")
+// }
+
+// Qno 6
+
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log("fizzbuzz")
+//     }
+
+
+// else if(i % 3 === 0){
+//     console.log("fizz")
+
+// }
+// else if(i % 5 === 0){
+//     console.log("buzz")
+
+// }
+// else{
+//     console.log(i)
+// }
+// }
+
+
+// Q no 7
+
+
+
